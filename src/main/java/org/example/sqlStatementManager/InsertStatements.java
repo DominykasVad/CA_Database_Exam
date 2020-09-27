60269a49cedb621921231f56e0bf4e7bab6193db
Dominykas Vad <domvadeisa@gmail.com>
2020-09-27 14:38:11 +0300

    updated InsertStatements

--- src/main/java/org/example/sqlStatementManager/InsertStatements.java
+++ src/main/java/org/example/sqlStatementManager/InsertStatements.java
@@ -6,4 +6,7 @@ public class InsertStatements {
 
     public static final String INSERT_NEW_USER = "INSERT INTO Users (username, password, name, surname, phone_number, email_address, account_iban)\n" +
             "VALUES (?, ?, ?, ?, ?, ?, ?);\n";
+
+    public static final String INSERT_NEW_TRANSACTION = "INSERT INTO Transactions (from_account_iban, to_account_iban, amount, date, time)\n" +
+            "VALUES (?, ?, ?, ?, ?);";
 }
