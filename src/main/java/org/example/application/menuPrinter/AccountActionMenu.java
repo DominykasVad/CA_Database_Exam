b355748a2cd76bfb63fc0ee2693be041e229e7af
Dominykas Vad <domvadeisa@gmail.com>
2020-09-27 14:42:41 +0300

    AccountActionMenu: minor fixes

--- src/main/java/org/example/application/menuPrinter/AccountActionMenu.java
+++ src/main/java/org/example/application/menuPrinter/AccountActionMenu.java
@@ -7,8 +7,9 @@ public class AccountActionMenu implements Printer {
         System.out.println("1. Įnešti pinigų");
         System.out.println("2. Išsiimti pinigus");
         System.out.println("3. Padaryti pavedimą");
-        System.out.println("4. Išeksportuoti transakcijų istoriją už norimą datą į failą");
-        System.out.println("5. Atsijungti");
+        System.out.println("4. Tranzakcijų istoriją");
+        System.out.println("5. Išeksportuoti transakcijų istoriją už norimą datą į failą");
+        System.out.println("6. Atsijungti");
         System.out.println("--------------------------------------------------");
     }
 }
