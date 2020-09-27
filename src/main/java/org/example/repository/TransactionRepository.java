0351d898ecf526689da34d94e7428c91ceaa6047
Dominykas Vad <domvadeisa@gmail.com>
2020-09-27 14:39:55 +0300

    implemented getTransactionByIbanList method

--- src/main/java/org/example/repository/TransactionRepository.java
+++ src/main/java/org/example/repository/TransactionRepository.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.example.sqlStatementManager.SelectStatements.SELECT_TRANSACTION_BY_IBAN;
 import static org.example.sqlStatementManager.SelectStatements.SELECT_TRANSACTION_BY_ID;
@@ -55,7 +57,44 @@ public class TransactionRepository {
         return transaction;
     }
 
-    public TransactionModel getTransactionByIban(String transactionIban) {
+    public List<TransactionModel> getTransactionByIbanList(String transactionIban) {
+        Connection connection = null;
+        PreparedStatement preparedStatement;
+        TransactionModel transaction = null;
+        List<TransactionModel> transactionList = new ArrayList<>();
+        try {
+            connection = databaseManager.getConnection();
+            preparedStatement = connection.prepareStatement(SELECT_TRANSACTION_BY_IBAN);
+            preparedStatement.setString(1, transactionIban);
+            preparedStatement.setString(2, transactionIban);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                transaction = new TransactionModelBuilder()
+                        .setId(resultSet.getInt("id"))
+                        .setFromAccount(resultSet.getString("from_account_iban"))
+                        .setToAccount(resultSet.getString("to_account_iban"))
+                        .setAmount(resultSet.getBigDecimal("amount"))
+                        .setDate(resultSet.getDate("date"))
+                        .setTime(resultSet.getTime("time"))
+                        .createTransactionModel();
+                transactionList.add(transaction);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return transactionList;
+    }
+
+    public TransactionModel getSignleTransactionByIban(String transactionIban) {
         Connection connection = null;
         PreparedStatement preparedStatement;
         TransactionModel transaction = null;
