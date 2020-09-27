47addf1dab3467de0f658f477ce2b0a1a6bf5c34
Dominykas Vad <domvadeisa@gmail.com>
2020-09-27 03:47:01 +0300

    implemented DepositHandler

---
+++ src/main/java/org/example/application/transactionHandler/DepositHandler.java
@@ -0,0 +1,43 @@
+package org.example.application.transactionHandler;
+
+import org.example.databaseManager.DatabaseManager;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.example.sqlStatementManager.UpdateStatements.UPDATE_ACCOUNT_BALANCE;
+
+public class DepositHandler {
+
+    private DatabaseManager databaseManager;
+
+    public DepositHandler() throws IOException {
+        this.databaseManager = DatabaseManager.getDatabaseManagerInstance();
+    }
+
+    public void handle(String accountIban, BigDecimal amount) {
+        Connection connection = null;
+        PreparedStatement preparedStatement;
+        try {
+            connection = databaseManager.getConnection();
+            preparedStatement = connection.prepareStatement(UPDATE_ACCOUNT_BALANCE);
+            preparedStatement.setBigDecimal(1, amount);
+            preparedStatement.setString(2, accountIban);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+}
